b2fb57742ac6af3b696d352b1481dd99ce698f64
hemeshvpatel <hemesh.v.patel@gmail.com>
2020-04-26 01:08:26 -0500

    modal

---
+++ src/navigation/HomeStack.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "../screens/HomeScreen";
+import AddRoomScreen from "../screens/AddRoomScreen";
+
+const Stack = createStackNavigator();
+const ChatAppStack = createStackNavigator();
+const ModalStack = createStackNavigator();
+
+function ChatApp() {
+  return (
+    <ChatAppStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#6646ee",
+        },
+        headerTintColor: "#ffffff",
+        headerTitleStyle: {
+          fontSize: 22,
+        },
+      }}
+    >
+      <ChatAppStack.Screen name="Home" component={HomeScreen} />
+    </ChatAppStack.Navigator>
+  );
+}
+
+export default function HomeStack() {
+  return (
+    <ModalStack.Navigator mode="modal" headerMode="none">
+      <ModalStack.Screen name="ChatApp" component={ChatApp} />
+      <ModalStack.Screen name="AddRoom" component={AddRoomScreen} />
+    </ModalStack.Navigator>
+  );
+}
